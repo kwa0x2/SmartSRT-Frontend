25505ada43f0b6d1da4660dd9eac88ad99cea35a
Alper Karakoyun <alperenibo14@gmail.com>
2025-02-09 14:19:26 +0300

    feat: file upload component

--- components/app/layout.tsx
+++ components/app/layout.tsx
@@ -9,7 +9,9 @@ const Layout = ({ children }: LayoutProps) => {
   return (
     <div className="min-h-dvh flex flex-col">
       <Header />
-      <main className="flex-1">{children}</main>
+      <main className="flex-1 flex items-center justify-center px-4 md:px-8">
+        <div className="w-full max-w-4xl mx-auto">{children}</div>
+      </main>
       <Footer />
     </div>
   );
