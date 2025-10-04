e9116774c3b9683d77c9434589fc68aa17a2afc4
Alper Karakoyun <alperenibo14@gmail.com>
2025-10-04 21:42:40 +0100

    feat: hero wave

--- components/home/hero/index.tsx
+++ components/home/hero/index.tsx
@@ -6,8 +6,15 @@ interface HeroProps {
 
 const Hero = ({isAuthenticated}: HeroProps) => {
   return (
-    <section id="hero" className="py-40 md:py-32 lg:py-64 min-w-screen">
-      <div className="px-4 md:px-8 lg:px-64 mx-auto">
+    <section id="hero" className="py-40 md:py-32 lg:py-64 min-w-screen relative">
+      <div
+        className="absolute inset-0 bg-center bg-no-repeat bg-contain opacity-30 pointer-events-none hidden md:block"
+        style={{
+          backgroundImage: "url('/images/hero-wave.svg')",
+          backgroundSize: "100% auto"
+        }}
+      />
+      <div className="px-4 md:px-8 lg:px-64 mx-auto relative z-10">
         <HeroContent isAuthenticated={isAuthenticated}/>
       </div>
     </section>
