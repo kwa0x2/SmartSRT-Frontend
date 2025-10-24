a76f098d9d3cf81bd29a1c4de81d7dcb1395b574
Alper Karakoyun <alperenibo14@gmail.com>
2025-10-24 23:09:20 +0100

    build: test 2

--- app/[locale]/(pages)/app/layout.tsx
+++ app/[locale]/(pages)/app/layout.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Ap766p",
+  description: "Upload your videos and generate professional SRT subtitles with AI",
+};
+
 export default function AppLayout({
   children,
 }: {
